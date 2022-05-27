57efde6bc66ab2ced27b3e02a317fa4e53940a39
francocortina <franco.cug@gmail.com>
2022-05-27 17:33:57 -0300

    byteCode

---
+++ src/main/java/com/bytecode/cursospringbytecode/CursoSpringByteCodeApplication.java
@@ -0,0 +1,13 @@
+package com.bytecode.cursospringbytecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringByteCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringByteCodeApplication.class, args);
+
+	}
+
---
+++ src/test/java/com/bytecode/cursospringbytecode/CursoSpringByteCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecode.cursospringbytecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringByteCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
